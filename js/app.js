05b356d82f5ca7a264d45fd3b782370327e28b7b
makhmud <admiral.happiness@gmail.com>
2014-09-26 00:13:23 +0300

    Basic elements

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+var widthAdapt = function() {
+	$('#top-panel').width( $('#page').width() - $('#left-sidebar').width() );
+	var finalContentOffset = $('#top-panel').width() - $('#order-list-wrapper').width();
+	if ( finalContentOffset>=864) {
+		finalContentOffset = 864;
+	}
+	$('#final-content').width(finalContentOffset);
+}
+
 $(document).ready( function() {
+
+	widthAdapt();
+
 	$('.scrollpane').jScrollPane();
+
+	
 })
+
+window.onresize = function () {
+	
+	widthAdapt();
+}
