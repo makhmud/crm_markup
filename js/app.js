873f628b3ffacdca1f2081610bf11fdb1aa53818
makhmud <admiral.happiness@gmail.com>
2014-09-26 23:52:33 +0300

    crm_grade (prepeared)

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var widthAdapt = function() {
 	$('#top-panel').width( $('#page').width() - $('#left-sidebar').width() );
 	var finalContentOffset = $('#top-panel').width() - $('#order-list-wrapper').width();
-	var maxWidth = 1104 - $('#order-list-wrapper').width();
+	var maxWidth = 1204 - $('#order-list-wrapper').width();
 	if ( finalContentOffset>=maxWidth) {
 		finalContentOffset = maxWidth;
 	}
