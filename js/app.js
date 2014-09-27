9c85ac64924535fa7a52ff4cf396ebbceae415f0
makhmud <admiral.happiness@gmail.com>
2014-09-27 10:45:24 +0300

    crm_delivery (prepeared)

--- js/app.js
+++ js/app.js
@@ -22,43 +22,45 @@ $(document).ready( function() {
 
 	$('.scrollpane').jScrollPane();
 
-	$('#filter-range, .input-date-range').datepick({ 
-	    rangeSelect: true, 
-	    dateFormat: 'dd.mm.yyyy',
-	    showTrigger: '.calendar-icon',
-	    prevText: '<', todayText: 'MM yyyy', nextText: '>',
-	    commandsAsDateFormat: true,
-	    pickerClass: 'popover gray',
-	    // onShow: function (){
-	    // 	console.log($('.calendar-icon').offset());
-	    // 	$('.datepick-popup').css({
-	    // 		left: $('.calendar-icon').offset().left + 'px',
-	    // 		top: $('.calendar-icon').offset().top + 'px'
-	    // 	});
-	    // },
-	    renderer: $.extend({}, $.datepick.defaultRenderer, 
-        {picker: $.datepick.defaultRenderer.picker. 
-            replace(/\{link:clear\}/, ''). 
-            replace(/\{link:close\}/, '')}), 
-	   //  onSelect: function (){
-	   //  	console.log($('.datepick-selected'));
-		  //   $('.datepick-selected').css({
-		  //   	'border-bottom-left-radius': '10px',
-				// 'border-top-left-radius': '10px'
-		  //   })
-	   //  }
-	});
-	$('.input-date-single').datepick({
-	    dateFormat: 'dd.mm.yyyy',
-	    showTrigger: '.input-date-single + .calendar-icon',
-	    prevText: '<', todayText: 'MM yyyy', nextText: '>',
-	    commandsAsDateFormat: true,
-	    pickerClass: 'popover gray',
-	    renderer: $.extend({}, $.datepick.defaultRenderer, 
-        {picker: $.datepick.defaultRenderer.picker. 
-            replace(/\{link:clear\}/, ''). 
-            replace(/\{link:close\}/, '')}), 
-	})
+	if (typeof( $().datepick ) != 'undefined'){
+		$('#filter-range, .input-date-range').datepick({ 
+		    rangeSelect: true, 
+		    dateFormat: 'dd.mm.yyyy',
+		    showTrigger: '.calendar-icon',
+		    prevText: '<', todayText: 'MM yyyy', nextText: '>',
+		    commandsAsDateFormat: true,
+		    pickerClass: 'popover gray',
+		    // onShow: function (){
+		    // 	console.log($('.calendar-icon').offset());
+		    // 	$('.datepick-popup').css({
+		    // 		left: $('.calendar-icon').offset().left + 'px',
+		    // 		top: $('.calendar-icon').offset().top + 'px'
+		    // 	});
+		    // },
+		    renderer: $.extend({}, $.datepick.defaultRenderer, 
+	        {picker: $.datepick.defaultRenderer.picker. 
+	            replace(/\{link:clear\}/, ''). 
+	            replace(/\{link:close\}/, '')}), 
+		   //  onSelect: function (){
+		   //  	console.log($('.datepick-selected'));
+			  //   $('.datepick-selected').css({
+			  //   	'border-bottom-left-radius': '10px',
+					// 'border-top-left-radius': '10px'
+			  //   })
+		   //  }
+		});
+		$('.input-date-single').datepick({
+		    dateFormat: 'dd.mm.yyyy',
+		    showTrigger: '.input-date-single + .calendar-icon',
+		    prevText: '<', todayText: 'MM yyyy', nextText: '>',
+		    commandsAsDateFormat: true,
+		    pickerClass: 'popover gray',
+		    renderer: $.extend({}, $.datepick.defaultRenderer, 
+	        {picker: $.datepick.defaultRenderer.picker. 
+	            replace(/\{link:clear\}/, ''). 
+	            replace(/\{link:close\}/, '')}), 
+		})
+}
 })
 
 window.onresize = function () {
